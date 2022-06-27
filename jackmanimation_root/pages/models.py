0d0b76004baee7e829d1336a384ffcc740a1c84a
Denis Jackman <denis_jackman@hotmail.com>
2022-06-27 15:29:59 +0100

    added an admin page

--- jackmanimation_root/pages/models.py
+++ jackmanimation_root/pages/models.py
@@ -1,3 +1,17 @@
+'''
+    django models class
+'''
 from django.db import models
 
 # Create your models here.
+class Page(models.Model):
+    '''
+        django page class
+    '''
+    title = models.CharField(max_length=60)
+    permalink = models.CharField(max_length=12, unique=True)
+    update_date = models.DateTimeField('Last Updated')
+    bodytext = models.TextField('Page Content', blank=True)
+
+    def __str__(self):
+        return self.title
